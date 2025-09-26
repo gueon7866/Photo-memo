c945284f20c57e3259569dde992ccee0250e55ab
gueon7866 <lsg7866@gmail.com>
2025-09-26 19:02:50 +0900

    1
    11

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const authRoutes = require("./routes/authRoutes")
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(cors({
+    origin: process.env.FRONT_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({ limit: "2mb" }));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB 연결 성공"))
+    .catch((err) => console.error("MongoDB 연결 실패:", err.message));
+
+app.get('/', (req, res) => {
+    res.send('Photo memo API CHECK OK!')
+})
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running ${PORT}`)
+})
